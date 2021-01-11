7f446524e6a0d096e06497f9ec002c01e682a43a
EsterRO <str0583237071@gmail.com>
2021-01-11 21:54:25 +0200

    11.01.21 by esty

--- Backend/dal/dal.js
+++ Backend/dal/dal.js
@@ -1,25 +1,25 @@
-const commonService= require("../common/service")
+const commonService = require("../common/service")
 
 const sql = require('mssql')
 
 var config = {
-    user: `MYMY\User`,
-     //password:123456789,
-    server: 'MYMY', 
-    database: 'learning_from_distance' ,
+    user: `sa`,
+    password: 'a1234',
+    server: 'localhost',
+    database: 'learning_from_distance',
     "options": {
         "encrypt": true,
         "enableArithAbort": true,
-        "tedious":true
-        }
+        "tedious": true
+    }
 };
 /* values, */
-function executeAsync(query,  values, res) {
-   
+function executeAsync(query, values, res) {
+
     return new Promise(function (resolve, reject) {
         sql.connect(config).then(() => {
             var request = new sql.Request();
-            request.query(query,function(err, result) {
+            request.query(query, function (err, result) {
                 if (err) {
                     reject(err);
                     //commonService.MySqlError(err, res)
@@ -42,4 +42,4 @@ module.exports = {
 
 
 
-     
+
